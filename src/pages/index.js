7485a5f61b9b6e046eb12735f852337c87943023
armagedon <abdo.vic@hotmail.com>
2020-03-15 17:20:57 +0100

    Info section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import { Link,graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/info"
 
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <BackgroundSection img={data.img.childImageSharp.fluid} title="regular joe's" styleClass="default-background" />
+    <Info />
   </Layout>
 )
 
