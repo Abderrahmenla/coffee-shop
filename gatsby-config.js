2aaf3c4e9ae5586f72e6b43eb8b758e70eb76961
armagedon <abdo.vic@hotmail.com>
2020-03-17 06:12:20 +0100

    menu section

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Regular Joe's`,
@@ -28,11 +32,19 @@ module.exports = {
       },
     },
     {
-  resolve: `gatsby-plugin-offline`,
-  options: {
-    precachePages: [`/about/`, `/`],
-  },
-},
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/about/`, `/`],
+        },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,15 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/info"
+import Menu from "../components/Home/Menu"
+
 
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <BackgroundSection img={data.img.childImageSharp.fluid} title="regular joe's" />
     <Info />
+    <Menu items={data.menu} />
   </Layout>
 )
 
@@ -22,6 +25,26 @@ export const query = graphql`
       }
     }
   }
+  menu: allContentfulCoffeeItem {
+    edges {
+      node {
+        id
+        title
+        description {
+          description
+        }
+        price
+        category
+        image {
+          fixed(width: 50, height: 50) {
+            ...GatsbyContentfulFixed_tracedSVG
+          }
+        }
+      }
+    }
+  }
+
+
 }
 
 `
