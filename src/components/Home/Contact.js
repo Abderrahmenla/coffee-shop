d101201ff054b003aba1e8a75d90c400b2c53475
armagedon <abdo.vic@hotmail.com>
2020-03-19 04:33:13 +0100

    adding form spree

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -7,7 +7,7 @@ export default function Contact () {
       <Title title="contact us" />
       <div className="row">
         <div className="col-10 col-sm-8 col-md-6 mx-auto">
-          <form>
+          <form action="https://formspree.io/xoqllkob" method="POST">
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" className="form-control" id="name" placeholder="abderrahmen lahmedi" />
