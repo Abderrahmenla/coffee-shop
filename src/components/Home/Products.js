baeee48a334999d58a366f46c47d859367ba7053
armagedon <abdo.vic@hotmail.com>
2020-03-19 06:08:09 +0100

    snipcart validation

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -13,6 +13,7 @@ const getProducts = graphql `
           price
           image {
             fluid(maxWidth: 426) {
+              src
               ...GatsbyContentfulFluid_tracedSVG
             }
           }
